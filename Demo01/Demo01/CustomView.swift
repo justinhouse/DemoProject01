b81a3237dd459ec3c1326b019599656becbc4259
justinhouse <justin@treehouse.nl>
2015-05-04 13:49:29 +0200

    Added custom view to project file

--- Demo01/Demo01/CustomView.swift
+++ Demo01/Demo01/CustomView.swift
@@ -6,4 +6,17 @@
 //  Copyright (c) 2015 Justin Hammenga. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class CustomView : UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder:aDecoder)
+    }
+    
+    
+}
